36d3098965f2b2d3d5610df1e26e4e57a26f761b
L3gendElias <jebarragan2021@gmail.com>
2025-07-26 19:21:12 -0600

    Second commit, fliping card added

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-const button = document.getElementById("MenuButton")
-const menudesplegable = document.getElementById("NavList")
-const enlaces = document.querySelectorAll(".nav__link")
+const button = document.getElementById("MenuButton");
+const menudesplegable = document.getElementById("NavList");
+const enlaces = document.querySelectorAll(".nav__link");
+const carta = document.getElementById("imagenlogo");
+const cartavolteada = document.getElementById("cartavolteada");
 
-button.addEventListener('click', () => {
-    menudesplegable.classList.toggle("active")
+button.addEventListener("click", () => {
+  menudesplegable.classList.toggle("active");
+});
+
+carta.addEventListener("click", () => {
+  carta.classList.toggle("active");
+  cartavolteada.classList.toggle("active");
+});
+
+cartavolteada.addEventListener("click", () => {
+  carta.classList.remove("active");
+  cartavolteada.classList.remove("active");
 });
 
 enlaces.forEach((enlace) => {
-    enlace.addEventListener("click", () => {
-        menudesplegable.classList.remove("active");
-    });
+  enlace.addEventListener("click", () => {
+    menudesplegable.classList.remove("active");
+  });
 });
